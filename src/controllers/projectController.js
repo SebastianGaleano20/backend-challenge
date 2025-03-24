6327b98df2287d1dc6b9468a0ebb75e30dd3d413
sebastiangaleano.dev <sebastiangaleano.dev@gmail.com>
2025-03-24 15:25:27 +0000

    projectController - getProjects created

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -1,7 +1,30 @@
 import HTTP_STATUS from '../helpers/httpStatus.js';
 import { createProjectSchema, updateProjectSchema } from '../schemas/usersSchema.js';
+import PrismaClient from '../config/prisma.js';
+
+const prisma = new PrismaClient();
 
 
-const projectController = () => { }
+const projectController = () => { 
+    const getProjects = async (req,res, next) => {
+        try {
+            const projects = await prisma.project.findMany();
+            const responseFormat = {
+                data: projects,
+                message: 'Projects fetched successfully',
+                status: HTTP_STATUS.OK,
+            }
+            return res.status(HTTP_STATUS.OK).json(responseFormat);
+        } catch (error) {
+            next(error);
+        } finally{
+            await prisma.$disconnect();
+        }
+    }
+
+    return {
+        getProjects,
+    }
+}
 
 export default projectController;
