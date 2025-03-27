4d855ef80be5fd36f2ed11072db90822037c199d
sebastiangaleano.dev <sebastiangaleano.dev@gmail.com>
2025-03-27 14:37:37 +0000

    SchemaValidator tiped

--- src/middlewares/schemaValidator.ts
+++ src/middlewares/schemaValidator.ts
@@ -1,7 +1,9 @@
+import Joi from "joi";
 import HTTP_STATUS from "../helpers/httpStatus";
+import { Response, Request, NextFunction } from "express";
 
-export const schemaValidator = (schema) => {
-    return (req, res, next) => {
+export const schemaValidator = (schema: Joi.Schema) => {
+    return (req: Request, res: Response, next: NextFunction) => {
         const { error } = schema.validate(req.body, { abortEarly: false });
         if (error) {
             const errorMessage = error.details.map((err) => err.message).join(', ');
