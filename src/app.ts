9cc173c24a1b2199bd26ea2e1e2fb3d0d68a01db
sebastiangaleano.dev <sebastiangaleano.dev@gmail.com>
2025-03-28 13:43:06 +0000

    swaggerUI added to server

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 import cors from "cors";
+
 
 dotenv.config();
 const SERVER_PORT = process.env.SERVER_PORT || 2010;
@@ -14,6 +17,7 @@ app.use(
     methods: "GET,POST,PATH,DELETE",
   })
 );
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJSDoc));
 
 app.listen(SERVER_PORT, () => {
   console.log(`Server on listening ${SERVER_PORT}`);
