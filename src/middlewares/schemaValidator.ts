a31e56fbf4e4418c8d41305306dc4c106b72741c
sebastiangaleano.dev <sebastiangaleano.dev@gmail.com>
2025-03-28 13:16:32 +0000

    swagger.yaml created

--- src/middlewares/schemaValidator.ts
+++ src/middlewares/schemaValidator.ts
@@ -2,15 +2,13 @@ import Joi from "joi";
 import HTTP_STATUS from "../helpers/httpStatus";
 import { Response, Request, NextFunction } from "express";
 
-export const schemaValidator = (schema: Joi.ObjectSchema) => {
+export const schemaValidator = (schema: Joi.ObjectSchema) => { // Recibimos el schema a validar
     return (req: Request, res: Response, next: NextFunction): void => {
-        const { error } = schema.validate(req.body, { abortEarly: false });
-        // Si hay un error, se maneja la respuesta
+        const { error } = schema.validate(req.body, { abortEarly: false }); // Capturamos el error
         if (error) {
             const errorMessage = error.details.map((err) => err.message).join(', ');
             res.status(HTTP_STATUS.BAD_REQUEST).json({ error: errorMessage });
         }
-        // Si no hay error, pasamos al siguiente middleware
-        next(); // Esto es lo que permite que Express continúe procesando la solicitud
+        next(); // Si no hay error sigue con el controlador
     };
 };
