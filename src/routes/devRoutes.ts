a309f3e51d2344eeb4b12f76d1f1ac5c34b7648f
SebastianGaleano20 <sebascarp9@gmail.com>
2025-04-01 22:00:18 -0300

    devSchema added to project succesfully

--- src/routes/devRoutes.ts
+++ src/routes/devRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { devController } from "@/controllers/devController";
+import { schemaValidator } from "@/middlewares/schemaValidator";
+import { devSchema } from "@/schemas/devSchema";
 
 export const devRouter = () => {
   const devRouter = Router();
   const { createDev } = devController();
-  devRouter.route("/dev").post(createDev);
+  devRouter.route("/dev").post(schemaValidator(devSchema), createDev);
   return devRouter;
 };
