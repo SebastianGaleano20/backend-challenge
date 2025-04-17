b8f5628536066d4b40317f698506cd6d675d1c5c
SebastianGaleano20 <sebascarp9@gmail.com>
2025-04-17 19:32:05 -0300

    Comments added to devController

--- src/controllers/devController.ts
+++ src/controllers/devController.ts
@@ -2,17 +2,20 @@ import { PrismaClient } from "@prisma/client";
 import httpStatus from "../helpers/httpStatus.js";
 import { Response, Request, NextFunction } from "express";
 import { formatResponse } from "../utils/formatResponse.js";
-
+// Iniciamos prismaClient
 const prisma = new PrismaClient();
 
 export const devController = () => {
+  // Servicio para crear Desarrollador
   const createDev = async (
     req: Request,
     res: Response,
     next: NextFunction
   ): Promise<void> => {
+    // Destructuramos los datos que vienen en el body
     const { name, email, image, role } = req.body;
     try {
+      // Creamos el desarrollador con el metodo create de prisma
       const developer = await prisma.developer.create({
         data: {
           name,
@@ -30,7 +33,11 @@ export const devController = () => {
       await prisma.$disconnect();
     }
   };
-  const getAllDev = async (req: Request, res: Response, next: NextFunction) => {
+  const getAllDev = async (
+    _req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
     try {
       const developers = await prisma.developer.findMany();
       res
