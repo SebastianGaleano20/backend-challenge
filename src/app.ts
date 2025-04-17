40ccc7015af7532d92f90f9867641e0c395673d0
SebastianGaleano20 <sebascarp9@gmail.com>
2025-04-17 20:11:21 -0300

    routes.json created - route information added to "/" URL

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import swaggerUi from "swagger-ui-express";
 import cors from "cors";
 import YAML from "yamljs";
+import fs from "fs";
 import { projectRouter } from "./routes/projectRoutes.js";
 import { devRouter } from "./routes/devRoutes.js";
 
@@ -10,6 +11,7 @@ dotenv.config();
 const SERVER_PORT = process.env.SERVER_PORT || 2010;
 const swaggerDoc = YAML.load("./src/docs/swagger.yaml");
 const app = express();
+const routes = JSON.parse(fs.readFileSync("./src/utils/routes.json", "utf8"));
 
 app.use(express.json());
 app.use(
@@ -18,9 +20,15 @@ app.use(
     methods: "GET,POST,PATCH,DELETE",
   })
 );
-
-app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.use("/api", projectRouter(), devRouter());
+app.use("/", (_req, res) => {
+  res.json({
+    message:
+      "Welcome to the Project Management API, these are the available routes",
+    routes: routes,
+  });
+});
 app.listen(SERVER_PORT, () => {
   console.log(`Server on listening ${SERVER_PORT}`);
 });
